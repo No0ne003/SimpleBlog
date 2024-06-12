11d7bbc336b87efd2c55573fb90f2eb924740f0d
No0ne003 <hi.yassir.op@gmail.com>
2024-06-12 11:40:59 +0100

    chore: header social links

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,7 +1,7 @@
 type IconProps = React.HTMLAttributes<SVGElement>;
 
-export const icons = {
-  logo: (props: IconProps) => {
+export const Icons = {
+  logo: (props: IconProps) => (
     <svg
       stroke="currentColor"
       fill="currentColor"
@@ -14,8 +14,8 @@ export const icons = {
     >
       <path d="M21 2H3a1 1 0 0 0-1 1v18a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zm-1 6h-4v4h4v4h-4v4h-4v-4H8v4H4v-4h4v-4H4V8h4V4h4v4h4V4h4v4z"></path>
       <path d="M8 8h4v4H8zm4 4h4v4h-4z"></path>
-    </svg>;
-  },
+    </svg>
+  ),
   twitter: (props: IconProps) => (
     <svg
       {...props}
