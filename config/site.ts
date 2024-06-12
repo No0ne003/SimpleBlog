65f3a0cc7de84e6436b7fbc319ceb44ba689936d
No0ne003 <hi.yassir.op@gmail.com>
2024-06-12 11:00:48 +0100

    feat: add config folder with site.tsx for site configuration

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "SimpleBlog",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "No0ne003",
+  links: {
+    github: "https://github.com/No0ne003",
+    twitter: "https://x.com/No0ne003",
+    instagram: "https://www.instagram.com/no0ne.003",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
