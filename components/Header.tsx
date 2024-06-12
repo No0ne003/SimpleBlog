6401dc0a81ded1afb7c58013e05980b9a21ee51f
No0ne003 <hi.yassir.op@gmail.com>
2024-06-12 11:42:25 +0100

    chore: header navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,13 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { Icons } from "./icons";
+import MainNav from "./Main-nav";
 
 const Header = () => {
   return (
     <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl item-center">
+        <MainNav />
         <div className="flex flex-1 items-center justify-end space-x-2">
           <nav className="flex items-center">
             <Link
---
+++ components/Main-nav.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { Icons } from "./icons";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+export function MainNav() {
+  const pathname = usePathname();
+  return (
+    <nav className="flex items-center space-x-4 lg:space-x-6">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        <Icons.logo className="h-6 w-6" />
+        <span className="font-bold">{siteConfig.name}</span>
+      </Link>
+      <Link
+        href="/blog"
+        className={cn(
+          "text-sm font-medium transition-colors hover:text-primary",
+          pathname === "/blog" ? "text-foreground" : "text-foreground/60",
+        )}
+      >
+        Blog
+      </Link>
+      <Link
+        href="/about"
+        className={cn(
+          "text-sm font-medium transition-colors hover:text-primary",
+          pathname === "/about" ? "text-foreground" : "text-foreground/60",
+        )}
+      >
+        about
+      </Link>
+    </nav>
+  );
+}
+
+export default MainNav;
