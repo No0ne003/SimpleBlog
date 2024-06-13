f65498a932c383ffe1be3fc41d2ca05b6f137029
No0ne003 <hi.yassir.op@gmail.com>
2024-06-13 15:21:14 +0100

    feat: complete post item

--- components/Post-item.tsx
+++ components/Post-item.tsx
@@ -1,3 +1,7 @@
+import { CalendarIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 interface PostItemProps {
   slug: string;
@@ -6,8 +10,32 @@ interface PostItemProps {
   date: string;
 }
 
-const PostItem = () => {
-  return <div>Post-item</div>;
+const PostItem = ({ slug, title, description, date }: PostItemProps) => {
+  return (
+    <article className="flex flex-col gap-2 border-border border-b py-3">
+      <div>
+        <h2 className="text-2xl font-bold">
+          <Link href={slug}>{title}</Link>
+        </h2>
+      </div>
+      <div className="max-w-none text-muted-foreground">{description}</div>
+      <div className="flex justify-between items-center">
+        <dl>
+          <dt className="sr-only">Published On</dt>
+          <dd className="text-sm sm:text-base font-medium flex items-center gap-1">
+            <CalendarIcon className="h-4 w-4" />
+            <time dateTime={date}>{date}</time>
+          </dd>
+        </dl>
+        <Link
+          href={slug}
+          className={cn(buttonVariants({ variant: "link" }), "py-0")}
+        >
+          Read more →
+        </Link>
+      </div>
+    </article>
+  );
 };
 
 export default PostItem;
