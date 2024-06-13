dc99d7014e560891bd29ab30eb9f2c568fccd1a4
No0ne003 <hi.yassir.op@gmail.com>
2024-06-13 15:13:06 +0100

    chore: setup post items and types

---
+++ components/Post-item.tsx
@@ -0,0 +1,13 @@
+
+interface PostItemProps {
+  slug: string;
+  title: string;
+  description?: string;
+  date: string;
+}
+
+const PostItem = () => {
+  return <div>Post-item</div>;
+};
+
+export default PostItem;
