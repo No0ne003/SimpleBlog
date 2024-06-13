32ff152cb4a7604e10e8f1c75476779b42a6e684
No0ne003 <hi.yassir.op@gmail.com>
2024-06-13 15:10:40 +0100

    chore: setup display posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,24 @@
+import { posts } from "#site/content";
+
 export default async function BlogPage() {
+  const displayPosts = posts;
+
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
-          <p className="text-xl text-muted-foreground">My rambliings on all things web dev.</p>
+          <p className="text-xl text-muted-foreground">
+            My rambliings on all things web dev.
+          </p>
         </div>
       </div>
+      <hr className="mt-8" />
+      {displayPosts?.length > 0 ? (
+        <ul className="flex flex-col"></ul>
+      ) : (
+        <p>Nothing to see here YET</p>
+      )}
     </div>
-  )
+  );
 }
