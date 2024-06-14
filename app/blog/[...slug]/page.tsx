7bfa3d67ba6b082fd491ad0e1e40c4c340b8b78e
No0ne003 <hi.yassir.op@gmail.com>
2024-06-14 11:24:56 +0100

    chore: getPostFromParams function

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,11 +1,21 @@
+import { posts } from "#site/content"
 interface PostPageProps {
   params: {
     slug: string[];
   };
 }
 
+async function getPostFromParams(params: PostPageProps["parmas"]) {
+  const slug = params?.slug?.join("/");
+  const post = posts.find(post => post.slugAsParams === slug)
+
+  return post;
+}
+
 export default async function PostPage({ params }: PostPageProps) {
   return (
-    <article className="container py-6 prose dark:prose-invert max-2-3xl mx-auto"></article>
+    <article className="container py-6 prose dark:prose-invert max-2-3xl mx-auto">
+      <h1 className="mb-2"></h1>
+    </article>
   );
 }
