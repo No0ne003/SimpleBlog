f76f4a40d937eff37f6fa723da03b97b4e772137
No0ne003 <hi.yassir.op@gmail.com>
2024-06-14 12:35:36 +0100

    fixing a bug and add code-blocks blog pos

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -28,7 +28,7 @@ export default async function PostPage({ params }: PostPageProps) {
   }
 
   return (
-    <article className="container py-6 prose dark:prose-invert max-2-3xl mx-auto">
+    <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
       <h1 className="mb-2 ">{post.title}</h1>
       {post.description ? (
         <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
