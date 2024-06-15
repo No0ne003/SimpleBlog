f7cd39ccb71118a730a1348e863ea51d594bdbda
No0ne003 <hi.yassir.op@gmail.com>
2024-06-15 13:24:15 +0100

    feat: latest posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,61 @@
 import { buttonVariants } from "@/components/ui/button";
 import { siteConfig } from "@/config/site";
-import { cn } from "@/lib/utils";
+import { cn, sortPosts } from "@/lib/utils";
 import Link from "next/link";
+import { posts } from "#site/content";
+import PostItem from "@/components/Post-item";
 
 export default function Home() {
+  const latestPosts = sortPosts(posts).slice(0, 3);
   return (
-    <section className="space-y-6 pb-8 md:pb-12 md:mt-10 lg:py-32">
-      <div className="container flex flex-col gap-4 text-center">
-        <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
-          Hello I&apos;m No0ne
-        </h1>
-        <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
-          Welcome to my blog template. Build using tailwind, shadcn, velite and
-          Nextjs 14
-        </p>
-        <div className="flex flex-col gap4 justify-center sm:flex-row">
-          <Link
-            href="/blog"
-            className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
-          >
-            View my blog
-          </Link>
-          <Link
-            href={siteConfig.links.github}
-            target="_blank"
-            rel="noreferrer"
-            className={cn(
-              buttonVariants({ variant: "outline", size: "lg" }),
-              "w-full sm:w-fit",
-            )}
-          >
-            Github
-          </Link>
+    <>
+      <section className="space-y-6 pb-8 md:pb-12 md:mt-10 lg:py-32">
+        <div className="container flex flex-col gap-4 text-center">
+          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
+            Hello I&apos;m No0ne
+          </h1>
+          <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
+            Welcome to my blog template. Build using tailwind, shadcn, velite
+            and Nextjs 14
+          </p>
+          <div className="flex flex-col gap4 justify-center sm:flex-row">
+            <Link
+              href="/blog"
+              className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
+            >
+              View my blog
+            </Link>
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                buttonVariants({ variant: "outline", size: "lg" }),
+                "w-full sm:w-fit",
+              )}
+            >
+              Github
+            </Link>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="container max-w-4xl py-4 lg:py-10 flex flex-col gap-8 mt-40">
+        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
+          Latest Posts
+        </h2>
+        <ul className="flex flex-col gap-10">
+          {latestPosts.map((post) => (
+            <li key={post.slug} className="first:border-t first:border-border">
+              <PostItem
+                slug={post.slug}
+                title={post.title}
+                description={post.description}
+                date={post.date}
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
 }
