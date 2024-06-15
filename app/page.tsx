76d58c4275c18fdda380f91ce8feebc25d444690
No0ne003 <hi.yassir.op@gmail.com>
2024-06-15 14:57:22 +0100

    style: change spacing between elements on hero section

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,18 @@ export default function Home() {
   return (
     <>
       <section className="space-y-6 pb-8 md:pb-12 md:mt-10 lg:py-32">
-        <div className="container flex flex-col gap-4 text-center">
-          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
-            Hello I&apos;m No0ne
-          </h1>
-          <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
-            Welcome to my blog template. Build using tailwind, shadcn, velite
-            and Nextjs 14
-          </p>
-          <div className="flex flex-col gap-4 justify-center sm:flex-row">
+        <div className="container flex flex-col gap-6 text-center">
+          <div className="space-y-2">
+            <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
+              Hello I&apos;m No0ne
+            </h1>
+            <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
+              Welcome to my blog template. Build using tailwind, shadcn, velite
+              and Nextjs 14
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-4 justify-center sm:flex-row ">
             <Link
               href="/blog"
               className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
