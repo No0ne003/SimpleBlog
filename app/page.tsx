c729a5c3af44618c0230a33093e351c861feca1c
No0ne003 <hi.yassir.op@gmail.com>
2024-06-15 13:08:21 +0100

    start hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <section>Hello World</section>;
+  return <section className="space-y-6 pb-8 md:pb-12 md:mt-10 lg:py-32">
+    <div className="container flex flex-col gap-4 text-center">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
+        Hello I&apos;m No0ne
+      </h1>
+      <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
+        Welcome to my blog template. Build using tailwind, shadcn, velite and Nextjs 14
+      </p>
+    </div>
+  </section>;
 }
