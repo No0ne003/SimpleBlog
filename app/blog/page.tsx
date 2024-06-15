b3ce37852d4939e59f62a4fb1eec552fe4392ac9
No0ne003 <hi.yassir.op@gmail.com>
2024-06-15 12:26:28 +0100

    chore: change displaying posts logic

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,9 +2,23 @@ import { posts } from "#site/content";
 import PostItem from "@/components/Post-item";
 import { sortPosts } from "@/lib/utils";
 
-export default async function BlogPage() {
+const POST_PER_PAGE = 5;
+
+interface BlogPageProps {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function BlogPage({ searchParams }: BlogPageProps) {
+  const currentPage = Number(searchParams?.page) || 1;
   const sortedPosts = sortPosts(posts.filter((post) => post.published));
-  const displayPosts = sortedPosts;
+  const totalPages = Math.ceil(sortedPosts.length / POST_PER_PAGE)
+
+  const displayPosts = sortedPosts.slice(
+    POST_PER_PAGE * (currentPage - 1),
+    POST_PER_PAGE * currentPage,
+  );
 
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
