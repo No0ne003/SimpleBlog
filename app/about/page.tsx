4fb6a500659112f6c59df81822732d7754b5b9c1
No0ne003 <hi.yassir.op@gmail.com>
2024-06-15 14:39:52 +0100

    chore: add information about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,12 +11,26 @@ export default async function About() {
       </div>
       <hr className="my-8 " />
       <div className="flex flex-col md:flex-row gap-8 items-center md:items-start">
-        <div className="min-w-48 max-w-48">
+        <div className="min-w-48 max-w-48 space-y-2">
           <Avatar className="size-48">
             <AvatarImage src="/pfp.jpg" alt={siteConfig.author} />
             <AvatarFallback>N0</AvatarFallback>
           </Avatar>
+          <div className="size-full">
+            <h2 className="text-2xl font-bold text-center break-words font-mono">
+              {siteConfig.author}
+            </h2>
+            <p className="text-muted-foreground text-center break-words">
+              Front-End Developer
+            </p>
+          </div>
         </div>
+        <p className="text-muted-foreground text-lg py-4 text-balance">
+          hello I&apos;m No0ne003 a Front-End developer from morocco, I start
+          with html on 08/26/2023 and I&apos;m currently practising Nextjs with
+          learn based project (like this one). I Want to start Back-End on 2024
+          summer.
+        </p>
       </div>
     </div>
   );
