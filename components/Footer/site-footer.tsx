c609ce9f0476887ff7a79d4314ca96d084b5b945
No0ne003 <hi.yassir.op@gmail.com>
2024-06-19 13:13:34 +0100

    refactor: improve footer

--- components/Footer/site-footer.tsx
+++ components/Footer/site-footer.tsx
@@ -3,30 +3,44 @@ import { EnvelopeClosedIcon } from "@radix-ui/react-icons";
 import { Icons } from "../icons";
 
 export function Footer() {
+  const socialLinks = [
+    {
+      href: "mailto:hi.yassir.op@gmail.com",
+      label: "Mail",
+      icon: <EnvelopeClosedIcon className="size-6" />,
+    },
+    {
+      href: siteConfig.links.twitter,
+      label: "Twitter",
+      icon: <Icons.twitter className="size-6" />,
+    },
+    {
+      href: siteConfig.links.github,
+      label: "Github",
+      icon: <Icons.gitHub className="size-6" />,
+    },
+    {
+      href: siteConfig.links.instagram,
+      label: "Instagram",
+      icon: <Icons.instagram className="size-6" />,
+    },
+  ];
+
   return (
     <footer>
       <div className="mb-6 mt-14 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
-          <a
-            target="_blank"
-            rel="noreforrer"
-            href="mainlto:hi.yassir.op@gmail.com"
-          >
-            <span className="sr-only">Mail</span>
-            <EnvelopeClosedIcon className="size-6" />
-          </a>
-          <a target="_blank" rel="noreforrer" href={siteConfig.links.twitter}>
-            <span className="sr-only">Twitter</span>
-            <Icons.twitter className="size-6" />
-          </a>
-          <a target="_blank" rel="noreforrer" href={siteConfig.links.github}>
-            <span className="sr-only">Github</span>
-            <Icons.gitHub className="size-6" />
-          </a>
-          <a target="_blank" rel="noreforrer" href={siteConfig.links.instagram}>
-            <span className="sr-only">Instagram</span>
-            <Icons.instagram className="size-6" />
-          </a>
+          {socialLinks.map(({ href, label, icon }) => (
+            <a
+              key={label}
+              target="_blank"
+              rel="noreferrer"
+              href={href}
+              aria-label={label}
+            >
+              {icon}
+            </a>
+          ))}
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-muted-foreground">
           <p>{siteConfig.author}</p>
