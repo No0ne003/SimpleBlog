1125ba00f4d065d4602248ce962c1663efe82c2e
No0ne003 <hi.yassir.op@gmail.com>
2024-06-19 12:52:09 +0100

    chore: add footer to layout

--- components/Footer/site-footer.tsx
+++ components/Footer/site-footer.tsx
@@ -1,7 +1,7 @@
-export function SiteFooter() {
-  return (
-  <footer>
-
-    </footer>
-  )
+export function Footer() {
+  return <footer>
+    <div className="mb-6 mt-14 flex flex-col items-center">
+      
+    </div>
+  </footer>;
 }
