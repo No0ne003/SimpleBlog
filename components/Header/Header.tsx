a90ac78f99c86850629da408b893cf70ee3a6cf4
No0ne003 <hi.yassir.op@gmail.com>
2024-06-19 13:19:53 +0100

    fix: fix header z-index

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -39,7 +39,7 @@ const SocialLink: React.FC<SocialLinkProps> = ({ href, label, Icon }) => (
 
 const Header: React.FC = () => {
   return (
-    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="z-10 sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <MainNav />
         <div className="flex flex-1 items-center justify-end space-x-2">
