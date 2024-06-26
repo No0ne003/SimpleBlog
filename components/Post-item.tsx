d1e109f19fb6e22f1aebfa86c71cadae2b6510c6
No0ne003 <hi.yassir.op@gmail.com>
2024-06-26 17:41:43 +0100

    fix: fix links of blog posts on tags page goto 404

--- components/Post-item.tsx
+++ components/Post-item.tsx
@@ -17,7 +17,7 @@ const PostItem = ({ slug, title, description, date, tags }: PostItemProps) => {
     <article className="flex flex-col gap-2 border-border border-b py-3">
       <div>
         <h2 className="text-2xl font-bold">
-          <Link href={slug}>{title}</Link>
+          <Link href={"/" + slug}>{title}</Link>
         </h2>
       </div>
       <div className="flex gap-2">
@@ -33,7 +33,7 @@ const PostItem = ({ slug, title, description, date, tags }: PostItemProps) => {
           </dd>
         </dl>
         <Link
-          href={slug}
+          href={"/" + slug}
           className={cn(buttonVariants({ variant: "link" }), "py-0")}
         >
           Read more →
