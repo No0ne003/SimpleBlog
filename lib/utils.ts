8da4b068b24770cc6542af6d25f7cb383f576cde
No0ne003 <hi.yassir.op@gmail.com>
2024-06-26 15:47:12 +0100

    chore: getAllTags util

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,3 +22,14 @@ export function sortPosts(posts: Array<Post>) {
     return 0;
   });
 }
+
+export function getAllTags(posts: Array<Post>) {
+  const tags: Record<string, number> = {};
+  posts.forEach((post) => {
+    post.tags?.forEach((tag) => {
+      tags[tag] = (tags[tag] ?? 0) + 1;
+    });
+  });
+
+  return tags;
+}
