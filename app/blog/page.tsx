e08e013f603d292eace019488a1f78f8f3446a1e
No0ne003 <hi.yassir.op@gmail.com>
2024-06-26 16:11:05 +0100

    feat: add tags on blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,9 @@
 import { posts } from "#site/content";
 import PostItem from "@/components/Post-item";
 import { QueryPagination } from "@/components/query-pagination";
-import { sortPosts } from "@/lib/utils";
+import { Tag } from "@/components/Tag";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getAllTags, sortPosts, sortTagsByCount } from "@/lib/utils";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -27,6 +29,9 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
     POST_PER_PAGE * currentPage,
   );
 
+  const tags = getAllTags(posts);
+  const sortedTags = sortTagsByCount(tags);
+
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
@@ -37,32 +42,46 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
           </p>
         </div>
       </div>
-      <hr className="mt-8" />
-      {displayPosts?.length > 0 ? (
-        <ul className="flex flex-col gap-5">
-          {displayPosts.map((post) => {
-            const { slug, date, title, description, tags } = post;
-            return (
-              <li key={slug}>
-                <PostItem
-                  slug={slug}
-                  title={title}
-                  date={date}
-                  description={description}
-                  tags={tags}
-                />
-              </li>
-            );
-          })}
-        </ul>
-      ) : (
-        <p>Nothing to see here YET</p>
-      )}
+      <div className="grid grid-cols-12 gap-3 mt-8">
+        <div className="col-span-12 col-start-1 sm:col-span-8">
+          <hr />
+          {displayPosts?.length > 0 ? (
+            <ul className="flex flex-col gap-5">
+              {displayPosts.map((post) => {
+                const { slug, date, title, description, tags } = post;
+                return (
+                  <li key={slug}>
+                    <PostItem
+                      slug={slug}
+                      title={title}
+                      date={date}
+                      description={description}
+                      tags={tags}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <p>Nothing to see here YET</p>
+          )}
 
-      <QueryPagination
-        totalPages={totalPages}
-        className="justify-center mt-8"
-      />
+          <QueryPagination
+            totalPages={totalPages}
+            className="justify-center mt-8"
+          />
+        </div>
+        <Card className="col-span-12 row-start-3 h-fit sm:col-span-4 sm:col-start-9 sm:row-start-1">
+          <CardHeader>
+            <CardTitle>Tags</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-wrap gap-2">
+            {sortedTags?.map((tag) => (
+              <Tag tag={tag} key={tag} count={tags[tag]} />
+            ))}
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
