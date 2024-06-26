fad578d3de2b1c89b8fe800b053046f884d93848
No0ne003 <hi.yassir.op@gmail.com>
2024-06-26 16:30:52 +0100

    chore: add header and style it

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -1,11 +1,24 @@
-import { Metadata } from "next"
-import {posts} from "#site/content"
+import { Metadata } from "next";
+import { posts } from "#site/content";
+import { getAllTags, sortTagsByCount } from "@/lib/utils";
 
 export const metadata: Metadata = {
-  title: 'Tags',
+  title: "Tags",
   description: "Topic I've writting about",
-}
+};
 
 export default async function TagsPage() {
-  const Tags = getAllTags(posts)
+  const tags = getAllTags(posts);
+  const sortedTags = sortTagsByCount(tags);
+
+  return (
+    <div className="container max-w-4xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-y-4">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">Tags</h1>
+        </div>
+      </div>
+      <hr className="my-4" />
+    </div>
+  );
 }
