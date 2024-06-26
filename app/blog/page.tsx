6ca0733eec40b5e6584ba31471fb79124dbd2aba
No0ne003 <hi.yassir.op@gmail.com>
2024-06-26 16:17:08 +0100

    style: change width of container of blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -33,7 +33,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
   const sortedTags = sortTagsByCount(tags);
 
   return (
-    <div className="container max-w-4xl py-6 lg:py-10">
+    <div className="container max-w-5xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
